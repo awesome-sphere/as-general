c4217357a5ec491f981316ae97132b210ec4396f
Vanessa5331 <earth5331@gmail.com>
2022-07-05 17:06:56 +0700

    Refactor dbURL format

--- models/setup.go
+++ models/setup.go
@@ -1,14 +1,30 @@
 package models
 
 import (
+	"fmt"
 	"log"
+
+	"github.com/awesome-sphere/as-general/utils"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() *gorm.DB {
-	dbURL := "postgres://pkinwza:securepassword@localhost:5432/as-cinema"
+	dbUser := utils.GetenvOr("POSTGRES_USER", "pkinwza")
+	dbPassword := utils.GetenvOr("POSTGRES_PASSWORD", "securepassword")
+	dbHost := utils.GetenvOr("POSTGRES_HOST", "localhost")
+	dbPort := utils.GetenvOr("POSTGRES_PORT", "5432")
+	dbName := utils.GetenvOr("POSTGRES_DB", "as-cinema")
+
+	dbURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName,
+	)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
