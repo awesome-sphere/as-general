0db7a905fed5eca650d6fbe16ca43429022a45a6
Vanessa5331 <earth5331@gmail.com>
2022-07-05 16:43:48 +0700

    Add GetenvOr util

--- assets/init.go
+++ assets/init.go
@@ -3,14 +3,16 @@ package assets
 import (
 	"log"
 
+	"github.com/awesome-sphere/as-general/utils"
+
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func ConnectMinio() {
 	endpoint := "localhost:9000"
-	accessKeyID := "minio"
-	secretAccessKey := "minio123"
+	accessKeyID := utils.GetenvOr("MINIO_ACCESS_KEY", "minio")
+	secretAccessKey := utils.GetenvOr("MINIO_SECRET_KEY", "minio123")
 	useSSL := false
 
 	minioClient, err := minio.New(endpoint, &minio.Options{
